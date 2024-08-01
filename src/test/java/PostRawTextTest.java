5d5acc0b97132d010e2034b86b85867991ba53ef
Dmitry Bushev <bushev.1997@mail.ru>
2024-08-01 19:47:31 +0400

    "first commit"

---
+++ src/test/java/PostRawTextTest.java
@@ -0,0 +1,27 @@
+import io.restassured.http.ContentType;
+import org.junit.Test;
+
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class PostRawTextTest {
+    @Test
+    public void postRawText() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .when()
+                .contentType(ContentType.TEXT)
+                .body("{\n    \"test\": \"value\"\n}")
+                .post("/post")
+                .then().log().all()
+                .assertThat()
+                .statusCode(200)
+                .body("args", anEmptyMap())
+                .body("data", equalTo("{\n    \"test\": \"value\"\n}"))
+                .body("files", anEmptyMap())
+                .body("form", anEmptyMap())
+                .body("json", nullValue())
+                .body("url", equalTo("https://postman-echo.com/post"));
+    }
+}
